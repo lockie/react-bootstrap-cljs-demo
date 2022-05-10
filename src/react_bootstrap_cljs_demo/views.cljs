b8e9ee669ce0f2339a51fc51215bddbdec489937
Andrew Kravchuk <awkravchuk@gmail.com>
2022-05-10 18:40:14 +0300

    Trying to use dropdown from react-bootstrap

--- src/react_bootstrap_cljs_demo/views.cljs
+++ src/react_bootstrap_cljs_demo/views.cljs
@@ -2,6 +2,11 @@
   (:require
    [re-frame.core :as re-frame]
    [react-bootstrap-cljs-demo.subs :as subs]
+   ["react-bootstrap/Button" :as Button]
+   ["react-bootstrap/Dropdown" :as Dropdown]
+   ["react-bootstrap/DropdownItem" :as DropdownItem]
+   ["react-bootstrap/DropdownMenu" :as DropdownMenu]
+   ["react-bootstrap/DropdownToggle" :as DropdownToggle]
    ))
 
 (defn main-panel []
@@ -9,4 +14,11 @@
     [:div
      [:h1
       "Hello from " @name]
+     [:> Button "Hit me"] ;; NOTE: this works
+     [:> Dropdown
+      [:> DropdownToggle "button"]
+      [:> DropdownMenu {:variant :dark}  ;; NOTE: this does not
+       [:> DropdownItem "action1"]
+       [:> DropdownItem "action2"]
+       [:> DropdownItem "action3"]]]
      ]))
